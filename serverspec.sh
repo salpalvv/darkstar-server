056125a7a4587a3095553910fc7ea2b2d3941f3d
Daniel Collette <gth701m@gmail.com>
2016-02-13 18:19:44 -0500

    serverspec debugging. closer

--- serverspec.sh
+++ serverspec.sh
@@ -4,18 +4,23 @@ sudo apt-get -y install git-core curl zlib1g-dev build-essential \
     sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev \
     python-software-properties libffi-dev
 
-pushd
 wget http://ftp.ruby-lang.org/pub/ruby/2.2/ruby-2.2.3.tar.gz
 tar -xzf ruby-2.2.3.tar.gz
 cd ruby-2.2.3/
+echo "./configure"
+echo "===================="
 ./configure > /dev/null 2>&1
+echo "make"
+echo "===================="
 make > /dev/null 2>&1
+echo "make install"
+echo "===================="
 sudo make install > /dev/null 2>&1
-popd
+cd ..
 
 ruby -v
 sudo gem install bundler serverspec
 
 cd serverspec
 sudo rake spec
-
+cd ..
