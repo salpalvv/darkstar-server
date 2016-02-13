61f3a02a8ee9b4629f71c48cb46bc3eb4b00d89f
Daniel Collette <gth701m@gmail.com>
2016-02-13 17:45:24 -0500

    serverspec debugging upgrading ruby on trusy bleh

--- serverspec.sh
+++ serverspec.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
-sudo gem install serverspec
+sudo apt-get -y install git-core curl zlib1g-dev build-essential \
+    libssl-dev libreadline-dev libyaml-dev libsqlite3-dev \
+    sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev \
+    python-software-properties libffi-dev
+
+cd
+wget http://ftp.ruby-lang.org/pub/ruby/2.2/ruby-2.2.3.tar.gz
+tar -xzvf ruby-2.2.3.tar.gz
+cd ruby-2.2.3/
+./configure
+make
+sudo make install
+ruby -v
+
+sudo gem install bundler serverspec
+
 cd serverspec
 sudo rake spec
 
