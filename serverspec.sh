427a03ff35a169ba029332df560b052ba48752de
Daniel Collette <gth701m@gmail.com>
2016-02-13 19:40:11 -0500

    serverspec debugging. issues with sudo

--- serverspec.sh
+++ serverspec.sh
@@ -7,19 +7,12 @@ sudo apt-get -y install git-core curl zlib1g-dev build-essential \
 wget http://ftp.ruby-lang.org/pub/ruby/2.2/ruby-2.2.3.tar.gz
 tar -xzf ruby-2.2.3.tar.gz
 cd ruby-2.2.3/
-echo "./configure"
-echo "===================="
 ./configure > /dev/null 2>&1
-echo "make"
-echo "===================="
 make > /dev/null 2>&1
-echo "make install"
-echo "===================="
-sudo make install > /dev/null 2>&1
+make install > /dev/null 2>&1
 cd ..
 
-ruby -v
-sudo gem install bundler serverspec
+gem install bundler serverspec
 
 cd serverspec
-sudo rake spec
+rake spec
