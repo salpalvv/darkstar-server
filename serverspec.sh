224a1cca5db5443d0dc43599d7e745e6874575f4
Daniel Collette <gth701m@gmail.com>
2016-02-13 21:39:48 -0500

    back to basics.

--- serverspec.sh
+++ serverspec.sh
@@ -14,5 +14,4 @@ cd ..
 
 sudo gem install bundler serverspec
 
-cd serverspec
 sudo rake spec
--- serverspec/spec/localhost_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe 'localhost' do
-  include_examples 'darkstar::init'
-end
