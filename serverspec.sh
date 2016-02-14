7543582bad690460fef37fc5b74d21ef39c94f6c
Daniel Collette <gth701m@gmail.com>
2016-02-13 20:21:18 -0500

    serverspec debugging. hmmm it was working :(

--- serverspec.sh
+++ serverspec.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get -y install git-core curl zlib1g-dev build-essential \
+apt-get -y install git-core curl zlib1g-dev build-essential \
     libssl-dev libreadline-dev libyaml-dev libsqlite3-dev \
     sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev \
     python-software-properties libffi-dev
